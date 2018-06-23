61e00014eef03c8b0fad98fae65a5405e9a3e2a2
Omar Faruk <omar@omarfarukmunna.tk>
2018-06-23 16:23:16 +0600

    multiple tcp connection handled

--- main.c
+++ main.c
@@ -14,7 +14,8 @@
 pthread_t listener_thread;
 
 
-void encodeData(char *msg, char *enc_data, int raw_data_len, int *enc_len){
+void encodeData(char *msg, char *enc_data, int raw_data_len, int *enc_len)
+{
 
     int i,j;
     uint8_t ms_bits;
@@ -33,7 +34,8 @@ void encodeData(char *msg, char *enc_data, int raw_data_len, int *enc_len){
 
 }
 
-void decodeData(char *encoded_data, char *decoded_data, int len,int *decoded_data_length){
+void decodeData(char *encoded_data, char *decoded_data, int len,int *decoded_data_length)
+{
 
     int i,j;
     uint8_t ms_bits=0;
@@ -52,8 +54,29 @@ void decodeData(char *encoded_data, char *decoded_data, int len,int *decoded_dat
     decoded_data[j]='\0';
 }
 
-void parser(char *data, int size){
-    puts(data);
+void *read_socket(void *sock_ptr)
+{
+    char buffer[1024];
+    char data[1024];
+    int data_length = 0;
+    int socket_fd = *((int *)sock_ptr);
+    free(sock_ptr);
+
+    printf("Communication socket: %d\n",socket_fd);
+
+    while(1)
+    {
+        read(socket_fd,buffer,1024);
+        decodeData(buffer,data,strlen(buffer),&data_length);
+
+        puts(data);
+        printf("Input for socket:%d\t:",socket_fd);
+        scanf("%s",buffer);
+        send(socket_fd,buffer,strlen(buffer),0);
+
+        memset(buffer,0,strlen(buffer));
+    }
+
 }
 
 void *listener()
@@ -64,78 +87,72 @@ void *listener()
     struct sockaddr_storage peer_address;
 	int opt = 1;
 	int addrlen = sizeof(address);
-	uint8_t buffer[1024] = {0};
 
 
 	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+    while(1){
 
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr("192.168.3.21");
-	address.sin_port = htons( PORT );
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
 
-	// Forcefully attaching socket to the port 6842
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = inet_addr("192.168.3.21");
+        address.sin_port = htons( PORT );
 
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-    int peer_add_len = sizeof(peer_address);
+        // Forcefully attaching socket to the port 6842
 
-    int ret = getpeername(new_socket, (struct sockaddr *)&peer_address, (socklen_t*)&peer_add_len);
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        int peer_add_len = sizeof(peer_address);
+
+        int ret = getpeername(new_socket, (struct sockaddr *)&peer_address, (socklen_t*)&peer_add_len);
 
 
-    if(ret < 0){
-        printf("%d\n",errno);
+        if(ret < 0){
+            printf("%d\n",errno);
+        }
+
+
+        int port;
+        char ipstr[20];
+        if (peer_address.ss_family == AF_INET) {
+            struct sockaddr_in *s = (struct sockaddr_in *)&peer_address;
+            port = ntohs(s->sin_port);
+            inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
+        }
+
+        printf("Peer IP address: %s\n", ipstr);
+
+        pthread_t new_socket_reader;
+        //continuously read the socket
+        int *socket_arg = malloc(sizeof(*socket_arg));
+        *socket_arg = new_socket;
+        pthread_create(&new_socket_reader,NULL,read_socket,socket_arg);
     }
 
 
-    int port;
-    char ipstr[20];
-    if (peer_address.ss_family == AF_INET) {
-        struct sockaddr_in *s = (struct sockaddr_in *)&peer_address;
-        port = ntohs(s->sin_port);
-        inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
-    }
-
-    printf("Peer IP address: %s\n", ipstr);
-
-
-    //continuously read the socket
-    char data[1024];
-    char data_length = 0;
-	while(1)
-    {
-        puts("thread running");
-        valread = read( new_socket , buffer, 1024);
-
-        decodeData(buffer,data,strlen(buffer),&data_length);
-        puts(data);
-
-        scanf("%s",buffer);
-        send(new_socket,buffer,strlen(buffer),0);
-
-	}
 
 }
 
